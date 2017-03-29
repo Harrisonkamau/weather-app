e9be36b0f55a9266ad38b82c777007e9a0458345
Harrisonkamau <kamauharrison87@gmail.com>
2017-03-29 15:57:55 +0300

    Create Koa App and set server config

---
+++ index.js
@@ -0,0 +1,25 @@
+var koa = require('koa');
+var json = require('koa-json');
+
+// create koa app
+var app = new koa();
+app.experimental = true
+
+// Local imports
+var router = require('./server/routes/index');
+var config = require('./server/config/config');
+
+
+app.use(json());
+
+// use the routes defined using the router
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+// Set default port
+var port = config.port || 5000;
+
+// start server
+app.listen(port, () => {
+  console.log("Server running on http://localhost:"+port);
+})
